a99293ece7a770ce265442da9701fe3d1d970c33
gevorg.kocharyan <gevorg.kocharyan@acba.am>
2025-07-30 21:44:36 +0400

    Add locators and utility method for login functionality
    Introduced specific locators for the login popup elements to enhance clarity and maintainability. Added a reusable `fillField` method in `AbsCommon` to simplify field interaction and reduce code duplication. These updates improve flexibility for managing login workflows.

--- src/main/java/common/AbsCommon.java
+++ src/main/java/common/AbsCommon.java
@@ -25,6 +25,12 @@ public class AbsCommon {
     return locator.textContent();
   }
 
+  public void fillField(Locator locator, String value) {
+    locator.scrollIntoViewIfNeeded();
+    waitForVisibility(locator);
+    locator.fill(value);
+  }
+
   public void click(Locator locator) {
     locator.scrollIntoViewIfNeeded();
     waitForVisibility(locator);
--- src/main/java/popups/LoginPopup.java
+++ src/main/java/popups/LoginPopup.java
@@ -1,11 +1,19 @@
 package popups;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import common.AbsCommon;
 
 public class LoginPopup extends AbsCommon {
 
+  Locator entrance = page.getByText("Вход");
+  Locator emailField = page.locator("//*[@id=\"__PORTAL__\"]//div[1]/div/input");
+  Locator passwordField = page.getByText("password");
+  Locator loginButton = page.locator("//*[@id=\"__PORTAL__\"]//div[1]/div/button/div");
+
   public LoginPopup(Page page) {
     super(page);
   }
+
+
 }
